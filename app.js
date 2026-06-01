50c2a549da9f2b1ffad2e3a2c98acf28ebde22fb
tetsu <yotsuba_1218@icloud.com>
2026-06-01 12:25:55 +0900

    メッセージ

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const pool = new Pool({
-  host:     process.env.DB_HOST,
-  port:     process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  user:     process.env.DB_USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
 
@@ -23,13 +23,17 @@ app.get('/api/messages', async (req, res) => {
 
 app.post('/api/messages', async (req, res) => {
   const { username, text } = req.body;
+
   const result = await pool.query(
     'INSERT INTO messages (username, text) VALUES ($1, $2) RETURNING *',
     [username, text]
   );
+
   res.json(result.rows[0]);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`サーバーが起動しました: http://localhost:${process.env.PORT || 3000}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("サーバーが起動しました: http://localhost:${process.env.PORT || 3000}");
 });
