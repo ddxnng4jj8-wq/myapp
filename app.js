6ab13ff8ee139d15b9d3799e7b655717c6f9c0ff
tetsu <yotsuba_1218@icloud.com>
2026-06-08 15:48:08 +0900

    メッセージ

--- app.js
+++ app.js
@@ -35,11 +35,3 @@ app.post('/api/messages', async (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log(`サーバーが起動しました: http://localhost:${process.env.PORT || 3000}`);
 });
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/chat.html');
-});
-
-app.get('/chat.html', (req, res) => {
-  res.sendFile(__dirname + '/public/chat.html');
-});
